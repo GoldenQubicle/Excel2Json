e6f1fe33feed92a23d24982f281a84720b466ec1
vrtxt <GoldenQubicle@gmail.com>
2017-05-18 15:05:49 +0200

    scaffold single sheet import

---
+++ Excel2Json/Import.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Excel = Microsoft.Office.Interop.Excel;
+using Newtonsoft.Json;
+
+namespace Excel2Json
+{
+    class Import
+    {
+        public void Read(string filename)
+        {
+            Excel.Application excel = new Excel.Application();
+            Excel.Workbook wb = excel.Workbooks.Open(filename);
+
+            // Get worksheet names
+            foreach (Excel.Worksheet sh in wb.Worksheets)
+                Console.WriteLine(sh.Name);
+
+
+
+            List<object> letters = new List<object>();
+
+            for (int cols = 3; cols < (cols + 11); cols++)
+            {
+                for (int rows = 5; rows < (rows + 11); rows++)
+                {
+                    letters.Add(wb.Sheets["makkelijk (makkelijk)"].Cells[cols, rows].Value2);
+                    Console.WriteLine(letters.Count);
+
+                }
+            }
+            
+            foreach (object letter in letters)
+            {
+                Console.WriteLine(letter);
+            }
+
+            wb.Close();
+            excel.Quit();
+        }
+    }
+
+
+}
+
--- Excel2Json/Program.cs
+++ Excel2Json/Program.cs
@@ -3,13 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Office.Interop.Excel;
+using Newtonsoft.Json;
 
 namespace Excel2Json
 {
     class Program
-    {
+    {            
         static void Main(string[] args)
         {
+            Import import = new Import();
+            import.Read("C:\\Users\\Erik\\Desktop\\TESTMAAND 1, WEEK 2 14 MEI VOLLEDIG\\MAAND 1, WEEK 2\\MAAND 1, WEEK 2, DAG 2\\WOORDZOEKER\\WZ makkelijk FRUIT.xlsx");
+
+            Console.Read();
         }
     }
 }
+
+
